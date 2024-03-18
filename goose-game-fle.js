5a9a5a60bdd9be2939d8cfb1f9e58048dec3d05f
Yahsin Huang <yahsinhuangtw@gmail.com>
2024-03-18 23:32:16 +0800

    goose game fle new

---
+++ goose-game-fle.js
@@ -0,0 +1,10 @@
+
+// On lance le dé. Returns a random integer from 0 to 6.
+Math.floor(Math.random() * 6);
+
+function lancer() {
+    Math.floor(Math.random() * 6);
+  };
+  
+  let result = lancer();
+  console.log(result); 
