1ed9e89e7782aa68dcb55d96040fe0c81dcc154f
Yahsin Huang <yahsinhuangtw@gmail.com>
2024-04-01 21:30:41 +0800

    make take turns work, commenting on plans of enterGameOver function and enterGameLoop function

--- scripts/goose-game-fle.js
+++ scripts/goose-game-fle.js
@@ -1,11 +1,11 @@
 let gameState = {
-  'State' : 'Game Loop',
-  'Turn' : 'playerGoldie',
-  beaglePosition : 0,
-  goldiePosition : 0,
- };
+  'State': 'Game Loop',
+  'Turn': 'playerBeagle',
+  beaglePosition: 0,
+  goldiePosition: 0,
+};
 
-let stateV =  gameState['State'];
+let stateV = gameState['State'];
 
 console.log("Here's the Game State: ", stateV);
 
@@ -31,7 +31,7 @@ document.getElementById("lancerB").onclick = function () {
 // After clicking c'est parti button, beaglePosition value adds diceTotal value, reassigns it to the newPosition. 
 document.getElementById("parti").onclick = function () {
   let diceTotal = document.getElementById("total").value;
-      diceTotal = parseInt(diceTotal);
+  diceTotal = parseInt(diceTotal);
   if (gameState['Turn'] === 'playerBeagle') {
     newPosition = gameState.beaglePosition + diceTotal;
     gameState.beaglePosition = newPosition;
@@ -40,21 +40,32 @@ document.getElementById("parti").onclick = function () {
     gameState.goldiePosition = newPosition;
   }
   console.log("Tell us the state of the game: ", gameState);
+  // Has anyone won? If beaglePosition or goldiePosition is greater than or equal to position 23, beagle or goldie has won; gameState enters gameover state. If not, game continues, gameState enters gameLoop state.
+  if (gameState.beaglePosition >= 23) {
+    console.log("Beagle has won. gameState enters gameover state.");
+  } else if (gameState.goldiePosition >= 23) {
+    console.log("Goldie has won. gameState enters gameover state.")
+  } else {
+    console.log("No one has won. gameState enters gameloop state.")
+  }
+
+  // Whose turn is it? if Turn equals to playerBeagle is True, update Turn to goldie, if Turn equals to playerBeagle is False, update Turn to beagle.
+
+  if (gameState.Turn === "playerBeagle") {
+    console.log("Update gameState.Turn to playerGoldie.");
+    gameState.Turn = 'playerGoldie';
+  } else {
+    console.log("Update gameState.Turn to playerBeagle.");
+    gameState.Turn = 'playerBeagle';
+  }
 };
+// write enterGameOver(){} function including hide lancerB, hide partiB, show restartB, hide "Résultat" form, display winner.
+// use div tag and id to put all of the game loop related ui elements such as button tags into one big div.
+// <div id = "gameover"> ... </div>
+// document.getElementById("gameover").style.sth -- to need loop it up.
 
-// Has anyone won? If beaglePosition or goldiePosition is greater than or equal to position 23, beagle or goldie has won; gameState enters gameover state. If not, game continues, gameState enters gameLoop state.
-if (gameState.beaglePosition >= 23) {
-  console.log("Beagle has won. gameState enters gameover state.");
-} else if (gameState.goldiePosition >= 23){
-  console.log("Goldie has won. gameState enters gameover state.")
-} else {
-  console.log("No one has won. gameState enters gameloop state.")
-}
 
-// Whose turn is it? if Turn equals to playerBeagle is True, update Turn to goldie, if Turn equals to playerBeagle is False, update Turn to beagle.
-
-if (gameState.Turn === "playerBeagle") {
-  console.log("Update gameState.Turn to playerGoldie.");
-} else {
-  console.log("Update gameState.Turn to playerBeagle.");
-}
+// write enterGameLoop(){} function including show lancerB, show partiB, show restartB, show "Résultat" form.
+// use div tag and id to put all of the game over related ui elements into one big div.
+// <div id = "gameloop"> ... </div>
+// document.getElementById("gameloop")
